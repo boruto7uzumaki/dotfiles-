c162a74caeff5c5d13b27b69b2798fb2e1f2b1f8
dezpair <55618993+boruto7uzumaki@users.noreply.github.com>
2022-08-17 20:40:34 +0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,6 +2,8 @@
 mv bspwmrc ~/.config/bspwm
 mv sxhkdrc ~/.config/sxhkd
 mv init.lua ~/.config/nvim
+mv launcher.sh ~/.config/rofi
+mv style-9.rasi ~/.config/rofi
 cp zathurarc ~/.config/zathura
 feh --bg-scale wall.jpg
 cp bashrc ~/bashrc
